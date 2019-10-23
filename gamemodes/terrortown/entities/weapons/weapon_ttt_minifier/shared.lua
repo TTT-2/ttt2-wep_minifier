989c10e0ebd40fffc93359b376cb9cfc34228f24
Tim Goll <tim@timgoll.de>
2019-10-23 22:54:37 +0200

    cone cleanup and doc

--- gamemodes/terrortown/entities/weapons/weapon_ttt_minifier/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_minifier/shared.lua
@@ -66,6 +66,7 @@ SWEP.NoSights = true
 if SERVER then
     util.AddNetworkString('ttt2_minifier_update_state')
 
+    -- helperfuncrion to start the use timer
     local function SetUpMinifyTimeouts(ply)
         local time = GetConVar('ttt_minifier_use_time'):GetInt()
 
@@ -80,6 +81,7 @@ if SERVER then
         end)
     end
 
+    -- helperfunction to start the cooldown timer
     local function SetUpUnMinifyTimeouts(ply)
         local time = GetConVar('ttt_minifier_cooldown_time'):GetInt()
 
@@ -100,24 +102,33 @@ if SERVER then
     end
 
     function MinifyPlayer(ply)
-        if not ply or not IsValid(ply) then return end
+        if not IsValid(ply) then return end
         if ply.minified then return end
 
+        -- set state variable
         ply.minified = true
+
+        -- sync state variable to client
         net.Start('ttt2_minifier_update_state')
         net.WriteBool(true)
         net.Send(ply)
 
+        -- change player health when minified
         ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor'))
         ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor'))
 
+        -- change the model and first person view cam
         ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor'))
         ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor'), 0.5)
         ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor'))
         ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor'))
 
+        -- increase gravity to decrease jumpheight
         ply:SetGravity(1.75)
 
+        -- change the hull of the player, this is needed since the model scaling only affects
+        -- the player-player hitbox, not the player-prop hitbox
+        -- only the z direction is changed to prevent glitching in walls when walking close to walls
         local hull_min, hull_max = ply:GetHull()
         hull_max.z = hull_max.z * GetGlobalFloat('ttt_minifier_factor')
         ply:SetHull(hull_min, hull_max)
@@ -126,35 +137,46 @@ if SERVER then
         hull_max_ducked.z = hull_max_ducked.z * GetGlobalFloat('ttt_minifier_factor')
         ply:SetHullDuck(hull_min_ducked, hull_max_ducked)
 
+        -- set timer to stop minier after a certain amount of time
         SetUpMinifyTimeouts(ply)
     end
 
+    -- reset the minifaction
     function UnMinifyPlayer(ply)
-        if not ply or not IsValid(ply) then return end
+        if not IsValid(ply) then return end
         if not ply.minified then return end
 
+        -- set state variable
         ply.minified = false
+
+        -- sync state variable to client
         net.Start('ttt2_minifier_update_state')
         net.WriteBool(false)
         net.Send(ply)
 
+        -- reset the health of the player
         ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor_inv'))
         ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor_inv'))
 
+        -- reset player model
         ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor_inv'), 0.5)
         ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor_inv'))
         ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor_inv'))
         ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor_inv'))
 
+        -- reset gravity
         ply:SetGravity(1)
 
+        -- reset player hitbox
         ply:ResetHull()
 
+        -- start timer for charge time
         SetUpUnMinifyTimeouts(ply)
     end
 
+    -- helperfunction to toggle between model size modes
     function ToggleMinifyPlayer(ply)
-        if not ply or not IsValid(ply) then return end
+        if not IsValid(ply) then return end
 
         if ply.minified then
             UnMinifyPlayer(ply)
@@ -166,21 +188,27 @@ if SERVER then
     function SWEP:Initialize()
         self.BaseClass.Initialize(self)
 
+        -- set speed multiplier to let players use the minifier instantly
         self:SetDeploySpeed(10)
     end
 
     function SWEP:PrimaryAttack()
+        -- disable minifier when cooldown is active
         if self.Owner.minify_cooldown then return end
 
+        -- set primary fire blocking time to prevent button spamming
         self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
     
+        -- minify player
         ToggleMinifyPlayer(self.Owner)
     end
-    
+
+    -- stop minification when minifier is dropped
     function SWEP:PreDrop()
         UnMinifyPlayer(self.Owner)
     end
-    
+
+    -- make sure that all player models are reset in the next round
     hook.Add('TTTPrepareRound', 'ttt2_minifier_speed_reset_all', function()
         for _, p in pairs(player.GetAll()) do
             UnMinifyPlayer(p)
@@ -189,15 +217,17 @@ if SERVER then
 end
 
 if CLIENT then
+    -- update minified state variable on the client as well since it is used for the SpeedMod hook
     net.Receive('ttt2_minifier_update_state', function()
         local client = LocalPlayer()
 
-        if not client or not IsValid(client) then return end
+        if not IsValid(client) then return end
 
         client.minified = net.ReadBool()
         client:EmitSound('minifing_player', 80)
     end)
 
+    -- register status icons
     hook.Add('Initialize', 'ttt2_dancegun_status_init', function() 
         STATUS:RegisterStatus('ttt2_minifier_active', {
             hud = Material('vgui/ttt/hud_icon_minified.png'),
@@ -210,8 +240,9 @@ if CLIENT then
     end)
 end
 
+-- change walk speed of minified players
 hook.Add('TTTPlayerSpeedModifier', 'ttt2_minifier_speed' , function(ply, _, _, noLag)
-    if not ply or not IsValid(ply) then return end
+    if not IsValid(ply) then return end
     if not ply.minified then return end
 
     noLag[1] = noLag[1] * GetGlobalFloat('ttt_minifier_factor')
