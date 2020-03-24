9863dcf5d222450ee826e3499c0ee11d92f02535
Tim Goll <tim@timgoll.de>
2020-03-24 16:56:48 +0100

    small cleanup

--- gamemodes/terrortown/entities/weapons/weapon_ttt_minifier/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_minifier/shared.lua
@@ -1,44 +1,44 @@
 if SERVER then
-    AddCSLuaFile()
+	AddCSLuaFile()
 
-    resource.AddFile('sound/minifier_shrink.wav')
+	resource.AddFile('sound/minifier_shrink.wav')
 
-    resource.AddFile('materials/vgui/ttt/icon_minifier')
-    resource.AddFile('materials/vgui/ttt/hud_icon_minified.png')
-    resource.AddFile('materials/vgui/ttt/hud_icon_minify_disabled.png')
+	resource.AddFile('materials/vgui/ttt/icon_minifier')
+	resource.AddFile('materials/vgui/ttt/hud_icon_minified.png')
+	resource.AddFile('materials/vgui/ttt/hud_icon_minify_disabled.png')
 end
 
 SWEP.Base = 'weapon_tttbase'
 
 if CLIENT then
-    hook.Add('Initialize', 'ttt2_minifier_init_language', function()
-        LANG.AddToLanguage('English', 'ttt2_weapon_minifier', 'Minifier')
-        LANG.AddToLanguage('Deutsch', 'ttt2_weapon_minifier', 'Verkleinerer')
-        
-        LANG.AddToLanguage('English', 'ttt2_weapon_minifier_desc', 'Use this item to shrink the size of your own body!')
-        LANG.AddToLanguage('Deutsch', 'ttt2_weapon_minifier_desc', 'Nutze dieses Item um deine Körpergröße zu verringern!')
-    end)
+	hook.Add('Initialize', 'ttt2_minifier_init_language', function()
+		LANG.AddToLanguage('English', 'ttt2_weapon_minifier', 'Minifier')
+		LANG.AddToLanguage('Deutsch', 'ttt2_weapon_minifier', 'Verkleinerer')
 
-    SWEP.Author = 'Mineotopia'
+		LANG.AddToLanguage('English', 'ttt2_weapon_minifier_desc', 'Use this item to shrink the size of your own body!')
+		LANG.AddToLanguage('Deutsch', 'ttt2_weapon_minifier_desc', 'Nutze dieses Item um deine Körpergröße zu verringern!')
+	end)
 
-    SWEP.ViewModelFOV = 54
-    SWEP.ViewModelFlip = false
+	SWEP.Author = 'Mineotopia'
 
-    SWEP.Category = 'Deagle'
-    SWEP.Icon = 'vgui/ttt/icon_minifier'
-    SWEP.EquipMenuData = {
-        type = 'item_weapon',
-        name = 'ttt2_weapon_minifier',
-        desc = 'ttt2_weapon_minifier_desc'
-    }
+	SWEP.ViewModelFOV = 54
+	SWEP.ViewModelFlip = false
 
-    sound.Add({
-        name = 'minifing_player',
-        channel = CHAN_STATIC,
-        volume = 1.0,
-        level = 130,
-        sound = 'minifier_shrink.wav'
-    })
+	SWEP.Category = 'Deagle'
+	SWEP.Icon = 'vgui/ttt/icon_minifier'
+	SWEP.EquipMenuData = {
+		type = 'item_weapon',
+		name = 'ttt2_weapon_minifier',
+		desc = 'ttt2_weapon_minifier_desc'
+	}
+
+	sound.Add({
+		name = 'minifing_player',
+		channel = CHAN_STATIC,
+		volume = 1.0,
+		level = 130,
+		sound = 'minifier_shrink.wav'
+	})
 end
 
 SWEP.Spawnable = false
@@ -64,188 +64,188 @@ SWEP.Primary.Ammo = 'none'
 SWEP.NoSights = true
 
 if SERVER then
-    util.AddNetworkString('ttt2_minifier_update_state')
+	util.AddNetworkString('ttt2_minifier_update_state')
 
-    -- helperfuncrion to start the use timer
-    local function SetUpMinifyTimeouts(ply)
-        local time = GetConVar('ttt_minifier_use_time'):GetInt()
+	-- helperfuncrion to start the use timer
+	local function SetUpMinifyTimeouts(ply)
+		local time = GetConVar('ttt_minifier_use_time'):GetInt()
 
-        STATUS:AddTimedStatus(ply, 'ttt2_minifier_active', time, true)
+		STATUS:AddTimedStatus(ply, 'ttt2_minifier_active', time, true)
 
-        ply.minifier_active_timer_id = 'minifier_active_timer_' .. tostring(CurTime())
+		ply.minifier_active_timer_id = 'minifier_active_timer_' .. tostring(CurTime())
 
-        timer.Create(ply.minifier_active_timer_id, time, 1, function()
-            if not ply or not IsValid(ply) then return end
+		timer.Create(ply.minifier_active_timer_id, time, 1, function()
+			if not ply or not IsValid(ply) then return end
 
-            UnMinifyPlayer(ply)
-        end)
-    end
+			UnMinifyPlayer(ply)
+		end)
+	end
 
-    -- helperfunction to start the cooldown timer
-    local function SetUpUnMinifyTimeouts(ply)
-        local time = GetConVar('ttt_minifier_cooldown_time'):GetInt()
+	-- helperfunction to start the cooldown timer
+	local function SetUpUnMinifyTimeouts(ply)
+		local time = GetConVar('ttt_minifier_cooldown_time'):GetInt()
 
-        STATUS:RemoveStatus(ply, 'ttt2_minifier_active')
-        STATUS:AddTimedStatus(ply, 'ttt2_minifier_cooldown', time, true)
+		STATUS:RemoveStatus(ply, 'ttt2_minifier_active')
+		STATUS:AddTimedStatus(ply, 'ttt2_minifier_cooldown', time, true)
 
-        if ply.minifier_active_timer_id then
-            timer.Remove(ply.minifier_active_timer_id)
-        end
+		if ply.minifier_active_timer_id then
+			timer.Remove(ply.minifier_active_timer_id)
+		end
 
-        ply.minifier_cooldown_timer_id = 'minifier_cooldown_timer_' .. tostring(CurTime())
-        ply.minify_cooldown = true
+		ply.minifier_cooldown_timer_id = 'minifier_cooldown_timer_' .. tostring(CurTime())
+		ply.minify_cooldown = true
 
-        timer.Create(ply.minifier_cooldown_timer_id, time, 1, function()
-            if not ply or not IsValid(ply) then return end
+		timer.Create(ply.minifier_cooldown_timer_id, time, 1, function()
+			if not ply or not IsValid(ply) then return end
 
-            STATUS:RemoveStatus(ply, 'ttt2_minifier_cooldown')
-            ply.minify_cooldown = false
-        end)
-    end
+			STATUS:RemoveStatus(ply, 'ttt2_minifier_cooldown')
+			ply.minify_cooldown = false
+		end)
+	end
 
-    function MinifyPlayer(ply)
-        if not IsValid(ply) then return end
-        if ply.minified then return end
+	function MinifyPlayer(ply)
+		if not IsValid(ply) then return end
+		if ply.minified then return end
 
-        -- set state variable
-        ply.minified = true
+		-- set state variable
+		ply.minified = true
 
-        -- sync state variable to client
-        net.Start('ttt2_minifier_update_state')
-        net.WriteBool(true)
-        net.Send(ply)
+		-- sync state variable to client
+		net.Start('ttt2_minifier_update_state')
+		net.WriteBool(true)
+		net.Send(ply)
 
-        -- change player health when minified
-        ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor'))
-        ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor'))
+		-- change player health when minified
+		ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor'))
+		ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor'))
 
-        -- change the model and first person view cam
-        ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor'))
-        ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor'), 0.5)
-        ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor'))
-        ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor'))
+		-- change the model and first person view cam
+		ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor'))
+		ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor'), 0.5)
+		ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor'))
+		ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor'))
 
-        -- increase gravity to decrease jumpheight
-        ply:SetGravity(1.75)
+		-- increase gravity to decrease jumpheight
+		ply:SetGravity(1.75)
 
-        -- change the hull of the player, this is needed since the model scaling only affects
-        -- the player-player hitbox, not the player-prop hitbox
-        -- only the z direction is changed to prevent glitching in walls when walking close to walls
-        local hull_min, hull_max = ply:GetHull()
-        hull_max.z = hull_max.z * GetGlobalFloat('ttt_minifier_factor')
-        ply:SetHull(hull_min, hull_max)
+		-- change the hull of the player, this is needed since the model scaling only affects
+		-- the player-player hitbox, not the player-prop hitbox
+		-- only the z direction is changed to prevent glitching in walls when walking close to walls
+		local hull_min, hull_max = ply:GetHull()
+		hull_max.z = hull_max.z * GetGlobalFloat('ttt_minifier_factor')
+		ply:SetHull(hull_min, hull_max)
 
-        local hull_min_ducked, hull_max_ducked = ply:GetHullDuck()
-        hull_max_ducked.z = hull_max_ducked.z * GetGlobalFloat('ttt_minifier_factor')
-        ply:SetHullDuck(hull_min_ducked, hull_max_ducked)
+		local hull_min_ducked, hull_max_ducked = ply:GetHullDuck()
+		hull_max_ducked.z = hull_max_ducked.z * GetGlobalFloat('ttt_minifier_factor')
+		ply:SetHullDuck(hull_min_ducked, hull_max_ducked)
 
-        -- set timer to stop minier after a certain amount of time
-        SetUpMinifyTimeouts(ply)
-    end
+		-- set timer to stop minier after a certain amount of time
+		SetUpMinifyTimeouts(ply)
+	end
 
-    -- reset the minifaction
-    function UnMinifyPlayer(ply)
-        if not IsValid(ply) then return end
-        if not ply.minified then return end
+	-- reset the minifaction
+	function UnMinifyPlayer(ply)
+		if not IsValid(ply) then return end
+		if not ply.minified then return end
 
-        -- set state variable
-        ply.minified = false
+		-- set state variable
+		ply.minified = false
 
-        -- sync state variable to client
-        net.Start('ttt2_minifier_update_state')
-        net.WriteBool(false)
-        net.Send(ply)
+		-- sync state variable to client
+		net.Start('ttt2_minifier_update_state')
+		net.WriteBool(false)
+		net.Send(ply)
 
-        -- reset the health of the player
-        ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor_inv'))
-        ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor_inv'))
+		-- reset the health of the player
+		ply:SetHealth(ply:Health() * GetGlobalFloat('ttt_minifier_factor_inv'))
+		ply:SetMaxHealth(ply:GetMaxHealth() * GetGlobalFloat('ttt_minifier_factor_inv'))
 
-        -- reset player model
-        ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor_inv'), 0.5)
-        ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor_inv'))
-        ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor_inv'))
-        ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor_inv'))
+		-- reset player model
+		ply:SetModelScale(ply:GetModelScale() * GetGlobalFloat('ttt_minifier_factor_inv'), 0.5)
+		ply:SetStepSize(ply:GetStepSize() * GetGlobalFloat('ttt_minifier_factor_inv'))
+		ply:SetViewOffset(ply:GetViewOffset() * GetGlobalFloat('ttt_minifier_factor_inv'))
+		ply:SetViewOffsetDucked(ply:GetViewOffsetDucked() * GetGlobalFloat('ttt_minifier_factor_inv'))
 
-        -- reset gravity
-        ply:SetGravity(1)
+		-- reset gravity
+		ply:SetGravity(1)
 
-        -- reset player hitbox
-        ply:ResetHull()
+		-- reset player hitbox
+		ply:ResetHull()
 
-        -- start timer for charge time
-        SetUpUnMinifyTimeouts(ply)
-    end
+		-- start timer for charge time
+		SetUpUnMinifyTimeouts(ply)
+	end
 
-    -- helperfunction to toggle between model size modes
-    function ToggleMinifyPlayer(ply)
-        if not IsValid(ply) then return end
+	-- helperfunction to toggle between model size modes
+	function ToggleMinifyPlayer(ply)
+		if not IsValid(ply) then return end
 
-        if ply.minified then
-            UnMinifyPlayer(ply)
-        else
-            MinifyPlayer(ply)
-        end
-    end
+		if ply.minified then
+			UnMinifyPlayer(ply)
+		else
+			MinifyPlayer(ply)
+		end
+	end
 
-    function SWEP:Initialize()
-        self.BaseClass.Initialize(self)
+	function SWEP:Initialize()
+		self.BaseClass.Initialize(self)
 
-        -- set speed multiplier to let players use the minifier instantly
-        self:SetDeploySpeed(10)
-    end
+		-- set speed multiplier to let players use the minifier instantly
+		self:SetDeploySpeed(10)
+	end
 
-    function SWEP:PrimaryAttack()
-        -- disable minifier when cooldown is active
-        if self.Owner.minify_cooldown then return end
+	function SWEP:PrimaryAttack()
+		-- disable minifier when cooldown is active
+		if self.Owner.minify_cooldown then return end
 
-        -- set primary fire blocking time to prevent button spamming
-        self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-    
-        -- minify player
-        ToggleMinifyPlayer(self.Owner)
-    end
+		-- set primary fire blocking time to prevent button spamming
+		self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 
-    -- stop minification when minifier is dropped
-    function SWEP:PreDrop()
-        UnMinifyPlayer(self.Owner)
-    end
+		-- minify player
+		ToggleMinifyPlayer(self.Owner)
+	end
 
-    -- make sure that all player models are reset in the next round
-    hook.Add('TTTPrepareRound', 'ttt2_minifier_speed_reset_all', function()
-        for _, p in pairs(player.GetAll()) do
-            UnMinifyPlayer(p)
-        end
-    end)
+	-- stop minification when minifier is dropped
+	function SWEP:PreDrop()
+		UnMinifyPlayer(self.Owner)
+	end
+
+	-- make sure that all player models are reset in the next round
+	hook.Add('TTTPrepareRound', 'ttt2_minifier_speed_reset_all', function()
+		for _, p in pairs(player.GetAll()) do
+			UnMinifyPlayer(p)
+		end
+	end)
 end
 
 if CLIENT then
-    -- update minified state variable on the client as well since it is used for the SpeedMod hook
-    net.Receive('ttt2_minifier_update_state', function()
-        local client = LocalPlayer()
+	-- update minified state variable on the client as well since it is used for the SpeedMod hook
+	net.Receive('ttt2_minifier_update_state', function()
+		local client = LocalPlayer()
 
-        if not IsValid(client) then return end
+		if not IsValid(client) then return end
 
-        client.minified = net.ReadBool()
-        client:EmitSound('minifing_player', 80)
-    end)
+		client.minified = net.ReadBool()
+		client:EmitSound('minifing_player', 80)
+	end)
 
-    -- register status icons
-    hook.Add('Initialize', 'ttt2_dancegun_status_init', function() 
-        STATUS:RegisterStatus('ttt2_minifier_active', {
-            hud = Material('vgui/ttt/hud_icon_minified.png'),
-            type = 'good'
-        })
-        STATUS:RegisterStatus('ttt2_minifier_cooldown', {
-            hud = Material('vgui/ttt/hud_icon_minify_disabled.png'),
-            type = 'bad'
-        })
-    end)
+	-- register status icons
+	hook.Add('Initialize', 'ttt2_dancegun_status_init', function()
+		STATUS:RegisterStatus('ttt2_minifier_active', {
+			hud = Material('vgui/ttt/hud_icon_minified.png'),
+			type = 'good'
+		})
+		STATUS:RegisterStatus('ttt2_minifier_cooldown', {
+			hud = Material('vgui/ttt/hud_icon_minify_disabled.png'),
+			type = 'bad'
+		})
+	end)
 end
 
 -- change walk speed of minified players
 hook.Add('TTTPlayerSpeedModifier', 'ttt2_minifier_speed' , function(ply, _, _, noLag)
-    if not IsValid(ply) then return end
-    if not ply.minified then return end
+	if not IsValid(ply) then return end
+	if not ply.minified then return end
 
-    noLag[1] = noLag[1] * GetGlobalFloat('ttt_minifier_factor')
+	noLag[1] = noLag[1] * GetGlobalFloat('ttt_minifier_factor')
 end)
--- lua/autorun/sh_minifier_convars.lua
+++ lua/autorun/sh_minifier_convars.lua
@@ -3,53 +3,53 @@ CreateConVar('ttt_minifier_cooldown_time', 15, {FCVAR_NOTIFY, FCVAR_ARCHIVE})
 CreateConVar('ttt_minifier_factor', 0.5, {FCVAR_NOTIFY, FCVAR_ARCHIVE})
 
 hook.Add('TTTUlxInitCustomCVar', 'ttt2_minifier_replicate_convars', function(name)
-    ULib.replicatedWritableCvar('ttt_minifier_use_time', 'rep_ttt_minifier_use_time', GetConVar('ttt_minifier_use_time'):GetInt(), true, false, name)
-    ULib.replicatedWritableCvar('ttt_minifier_cooldown_time', 'rep_ttt_minifier_cooldown_time', GetConVar('ttt_minifier_cooldown_time'):GetInt(), true, false, name)
-    ULib.replicatedWritableCvar('ttt_minifier_factor', 'rep_ttt_minifier_factor', GetConVar('ttt_minifier_factor'):GetFloat(), true, false, name)
+	ULib.replicatedWritableCvar('ttt_minifier_use_time', 'rep_ttt_minifier_use_time', GetConVar('ttt_minifier_use_time'):GetInt(), true, false, name)
+	ULib.replicatedWritableCvar('ttt_minifier_cooldown_time', 'rep_ttt_minifier_cooldown_time', GetConVar('ttt_minifier_cooldown_time'):GetInt(), true, false, name)
+	ULib.replicatedWritableCvar('ttt_minifier_factor', 'rep_ttt_minifier_factor', GetConVar('ttt_minifier_factor'):GetFloat(), true, false, name)
 end)
 
 if SERVER then
-    -- ConVar replication is broken in GMod, so we do this, at least Alf added a hook!
-    -- I don't like it any more than you do, dear reader. Copycat!
-    hook.Add('TTT2SyncGlobals', 'ttt2_minifier_sync_convars', function()
-        SetGlobalFloat('ttt_minifier_factor', GetConVar('ttt_minifier_factor'):GetFloat())
-        SetGlobalFloat('ttt_minifier_factor_inv', 1 / GetConVar('ttt_minifier_factor'):GetFloat())
-    end)
+	-- ConVar replication is broken in GMod, so we do this, at least Alf added a hook!
+	-- I don't like it any more than you do, dear reader. Copycat!
+	hook.Add('TTT2SyncGlobals', 'ttt2_minifier_sync_convars', function()
+		SetGlobalFloat('ttt_minifier_factor', GetConVar('ttt_minifier_factor'):GetFloat())
+		SetGlobalFloat('ttt_minifier_factor_inv', 1 / GetConVar('ttt_minifier_factor'):GetFloat())
+	end)
 
-    -- sync convars on change
-    cvars.AddChangeCallback('ttt_minifier_factor', function(cv, old, new)
-        SetGlobalFloat('ttt_minifier_factor', tonumber(new))
-        SetGlobalFloat('ttt_minifier_factor_inv', 1 / tonumber(new))
-    end)
+	-- sync convars on change
+	cvars.AddChangeCallback('ttt_minifier_factor', function(cv, old, new)
+		SetGlobalFloat('ttt_minifier_factor', tonumber(new))
+		SetGlobalFloat('ttt_minifier_factor_inv', 1 / tonumber(new))
+	end)
 end
 
 -- add to ULX
 if CLIENT then
-    hook.Add('TTTUlxModifyAddonSettings', 'ttt2_minifier_add_to_ulx', function(name)
-        local tttrspnl = xlib.makelistlayout{w = 415, h = 318, parent = xgui.null}
+	hook.Add('TTTUlxModifyAddonSettings', 'ttt2_minifier_add_to_ulx', function(name)
+		local tttrspnl = xlib.makelistlayout{w = 415, h = 318, parent = xgui.null}
 
-        -- Basic Settings
-        local tttrsclp = vgui.Create('DCollapsibleCategory', tttrspnl)
-        tttrsclp:SetSize(390, 75)
-        tttrsclp:SetExpanded(1)
-        tttrsclp:SetLabel('Basic Settings')
+		-- Basic Settings
+		local tttrsclp = vgui.Create('DCollapsibleCategory', tttrspnl)
+		tttrsclp:SetSize(390, 75)
+		tttrsclp:SetExpanded(1)
+		tttrsclp:SetLabel('Basic Settings')
 
-        local tttrslst = vgui.Create('DPanelList', tttrsclp)
-        tttrslst:SetPos(5, 25)
-        tttrslst:SetSize(390, 75)
-        tttrslst:SetSpacing(5)
+		local tttrslst = vgui.Create('DPanelList', tttrsclp)
+		tttrslst:SetPos(5, 25)
+		tttrslst:SetSize(390, 75)
+		tttrslst:SetSpacing(5)
 
-        local tttslid1 = xlib.makeslider{label = 'ttt_minifier_use_time (def. 25)', repconvar = 'rep_ttt_minifier_use_time', min = 0, max = 100, decimal = 0, parent = tttrslst}
-        tttrslst:AddItem(tttslid1)
+		local tttslid1 = xlib.makeslider{label = 'ttt_minifier_use_time (def. 25)', repconvar = 'rep_ttt_minifier_use_time', min = 0, max = 100, decimal = 0, parent = tttrslst}
+		tttrslst:AddItem(tttslid1)
 
-        local tttslid2 = xlib.makeslider{label = 'ttt_minifier_cooldown_time (def. 15)', repconvar = 'rep_ttt_minifier_cooldown_time', min = 0, max = 100, decimal = 0, parent = tttrslst}
-        tttrslst:AddItem(tttslid2)
+		local tttslid2 = xlib.makeslider{label = 'ttt_minifier_cooldown_time (def. 15)', repconvar = 'rep_ttt_minifier_cooldown_time', min = 0, max = 100, decimal = 0, parent = tttrslst}
+		tttrslst:AddItem(tttslid2)
 
-        local tttslid3 = xlib.makeslider{label = 'ttt_minifier_factor (def. 0.5)', repconvar = 'rep_ttt_minifier_factor', min = 0.01, max = 2, decimal = 2, parent = tttrslst}
-        tttrslst:AddItem(tttslid3)
+		local tttslid3 = xlib.makeslider{label = 'ttt_minifier_factor (def. 0.5)', repconvar = 'rep_ttt_minifier_factor', min = 0.01, max = 2, decimal = 2, parent = tttrslst}
+		tttrslst:AddItem(tttslid3)
 
-        -- add to ULX
-        xgui.hookEvent('onProcessModules', nil, tttrspnl.processModules)
-        xgui.addSubModule('Minifier', tttrspnl, nil, name)
-    end)
+		-- add to ULX
+		xgui.hookEvent('onProcessModules', nil, tttrspnl.processModules)
+		xgui.addSubModule('Minifier', tttrspnl, nil, name)
+	end)
 end
