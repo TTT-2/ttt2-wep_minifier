d09bf7cf928c8bc56dc06ec165780f4b9a4cc423
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 17:06:33 +0500

    Create minifier.lua (#2)

---
+++ lua/terrortown/lang/russian/minifier.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt2_weapon_minifier"] = "Минификатор"
+L["ttt2_weapon_minifier_desc"] = "Используйте этот предмет, чтобы уменьшить размер собственного тела!"
+L["ttt2_weapon_minifier_help_msb1"] = "Переключение между уменьшенным и нормальным размером игрока"
