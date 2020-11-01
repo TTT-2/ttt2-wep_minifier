48e2886edc23e2a3024f9c647709fe74862ca942
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 03:06:30 +0100

    Create minifier.lua
    french translation minifier

---
+++ lua/terrortown/lang/french/minifier.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["ttt2_weapon_minifier"] = "Réducteur de taille"
+L["ttt2_weapon_minifier_desc"] = "Utilisez cet objet pour réduire la taille de votre propre corps!"
+L["ttt2_weapon_minifier_help_msb1"] = "Échanger entre la taille miniature et la taille normale"
